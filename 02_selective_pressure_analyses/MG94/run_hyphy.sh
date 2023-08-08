630de2d942c5ea595c38716634be1087ed22f4aa
PeterMulhair <bspm@leeds.ac.uk>
2023-08-08 11:07:29 +0100

    Add data for FitMG94 test

---
+++ 02_selective_pressure_analyses/MG94/run_hyphy.sh
@@ -0,0 +1,2 @@
+#Command used to run hyphy FitMG94 analysis on Blue opsin alignment
+~/bin/hyphy/hyphy /home/zoo/zool2500/bin/hyphy-analyses/FitMG94/FitMG94.bf --alignment codon_align/lep_blue_nucl_trim_aln.fasta --tree gene-trees/lep_blue_nucl_trim_aln.nwk --type local
